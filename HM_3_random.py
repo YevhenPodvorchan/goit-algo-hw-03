24e257f9b75d96f25b8ca5b98e11a65b891e8cf1
YevhenPodvorchan <Evgen585rih@gmail.com>
2025-01-03 17:51:26 +0100

    Update HM_3_random.py

--- HM_3_random.py
+++ HM_3_random.py
@@ -1,12 +1,9 @@
 import random
 
-
-def get_numbers_tiket(min, max, quantity):
-
+def get_numbers_ticket(min, max, quantity):
         if min >= max or quantity <= 0 or quantity >= (max - min + 1):  #перевіряємо валідність данних
             return []  #як є помилка повертаємо пустий список
-
         return sorted(random.sample(range(min, max +1), quantity))  #random.sample генерує унікальні числа без додаткових перевірок sorted відсортовані
 
-lottery_numers = get_numbers_tiket(1, 49, 6)  #задаємо параметри розіграшу лотереї
+lottery_numers = get_numbers_ticket(1, 49, 6)
 print("Виграшні числа лотереї:", lottery_numers)
